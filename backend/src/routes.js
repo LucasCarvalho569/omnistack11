6113552d1cf1d7a001a37e9f6ba4aec40e59c4bd
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-03-26 09:16:42 -0300

    OngRoutes and OngController

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const routes = express.Router();
+const OngController = require("./controllers/OngController");
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+module.exports = routes;
